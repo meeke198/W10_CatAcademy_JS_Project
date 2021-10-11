fa923fd38caa31baa9128fca0e6c7d0073c9f45e
hien nguyen <thuhiennguyen198@gmail.com>
2021-10-10 22:26:06 -0400

    restructure skeleton

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import Game from './game';
+
+document.addEventListener("DOMContentLoaded", () => {
+    let game = new Game();
+
+    let startButton = document.getElementById("start-button");
+    let playAgainButton = document.getElementById("play-again-button");
+
+    startButton.addEventListener("click", () => {
+        document.getElementById("introduction").classList.add("hidden");
+        game.start();
+    })
+
+    playAgainButton.addEventListener("click", () => {
+        game.restart();
+    })
+});
