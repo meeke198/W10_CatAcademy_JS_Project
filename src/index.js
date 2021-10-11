1205fc122e1f97380ea6638fa159f7e665e21c33
hien nguyen <thuhiennguyen198@gmail.com>
2021-10-10 22:53:34 -0400

    draw blackboard canvas

--- src/index.js
+++ src/index.js
@@ -7,3 +7,8 @@ canvas.height = 3200;
 canvas.style.width = "800px"; //kich thuoc show trong html
 canvas.style.height = "600px";
 canvas.style.background = "#999999"; 
+
+const boardImg = new Image(); // khong xai const se thanh global, khong can xai this vi khong can xai o cho khac
+boardImg.src = "assest/icons/board.jpeg"
+boardImg.onload = function () { //cho cai hinh load len het roi moi chay function nay
+ctx.drawImage(boardImg, 400, 600, 2400, 1400)};
