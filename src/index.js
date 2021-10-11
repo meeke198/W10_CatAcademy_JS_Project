d017eca0803c3f4a04ee8bc8814a26240a4ce764
hien nguyen <thuhiennguyen198@gmail.com>
2021-10-10 22:48:32 -0400

    add canvas

--- src/index.js
+++ src/index.js
@@ -1,17 +1,9 @@
-import Game from './game';
-
-document.addEventListener("DOMContentLoaded", () => {
-    let game = new Game();
-
-    let startButton = document.getElementById("start-button");
-    let playAgainButton = document.getElementById("play-again-button");
-
-    startButton.addEventListener("click", () => {
-        document.getElementById("introduction").classList.add("hidden");
-        game.start();
-    })
-
-    playAgainButton.addEventListener("click", () => {
-        game.restart();
-    })
-});
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+// ctx.fillStyle = 'green';
+// ctx.fillRect(10, 10, 150, 100);
+canvas.width = 3200; //kich thuoc that
+canvas.height = 3200;
+canvas.style.width = "800px"; //kich thuoc show trong html
+canvas.style.height = "600px";
+canvas.style.background = "#999999"; 
